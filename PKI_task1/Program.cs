0efc0805c805e8f6cf9156edd9ce062894e30ee0
Seyn <hub6dom99@gmail.com>
2021-03-03 17:58:53 +0100

    zmiana mnożenia w metodzie policz

--- PKI_task1/Program.cs
+++ PKI_task1/Program.cs
@@ -6,7 +6,12 @@ namespace PKI_task1
     {
         static void Main(string[] args)
         {
-        
+            Console.WriteLine($"W pierwszym branchu; 2+3={Policz(2, 3)}");
+        }
+
+        static int Policz(int a, int b)
+        {
+            return a * b;
         }
     }
 }
